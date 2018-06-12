29a318ae2e734e0da7c3ecf73705abe3fda6e483
Abdul-Latif Sumani <latifelnino@yahoo.com>
2018-06-12 14:58:59 +0000

    Added bootstap and style.css

--- models/db.py
+++ models/db.py
@@ -3,7 +3,7 @@ configuration = AppConfig(reload=True)
 
 db = DAL("sqlite://storage.sqlite")
 db.define_table("users",
-                Field('db_firstname'),
-                Field('db_lastname'),
-                Field('db_email'),
-                Field('db_password'))
+               Field('db_firstname'),
+               Field('db_lastname'),
+               Field('db_email'),
+               Field('db_password'))
